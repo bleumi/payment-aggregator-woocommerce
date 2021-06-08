94ab521d9dc09ded111aef1eac793a77238982db
Karthic Kumaran K <karthic.krisnakumar@bleumi.com>
2021-06-08 00:38:42 +0000

    bump

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name:  Bleumi Payments for WooCommerce
  * Description:  Accept Traditional and Crypto Currency Payments
- * Version:      1.0.1
+ * Version:      1.0.2
  * Author:       Bleumi Inc
  * Author URI:   https://bleumi.com/
  * License:      Copyright 2020 Bleumi, MIT License
